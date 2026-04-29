a7c107d7689e83f00973329163cd5ebd626aa059
Prince Lumina <smahdih71@gmail.com>
2026-04-29 09:35:01 +0200

    Add limit and offset

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: run
+.PHONY: run lint
 
 run:
 	uv run fastapi dev app/main.py
+
+lint:
+	@uv run ruff check --select I --fix
+	@uv run ruff check --fix
--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-from http.client import HTTPException
-
 import requests
 from fastapi import FastAPI, HTTPException
 
@@ -7,8 +5,13 @@ app = FastAPI()
 
 USERS_API_URL = "https://dummyjson.com/users"
 
+
 @app.get("/users")
-def read_users(min_age: int | None = None):
+def read_users(
+    min_age: int | None = None,
+    limit: int | None = None,
+    offset: int | None = None,
+):
     if min_age is not None and min_age < 0:
         raise HTTPException(status_code=400, detail="Min age cannot be negative")
     try:
@@ -21,7 +24,7 @@ def read_users(min_age: int | None = None):
         ) from exc
     payload = response.json()
     users = payload.get("users", [])
-    return [
+    out = [
         {
             "id": user.get("id"),
             "full_name": f"{user.get('firstName', '')} {user.get('lastName', '')}".strip(),
@@ -30,5 +33,16 @@ def read_users(min_age: int | None = None):
             "company": user.get("company", {}).get("name"),
             "country": user.get("address", {}).get("country"),
         }
-        for user in users if min_age is None or user.get("age") >= min_age
+        for user in users
+        if min_age is None or user.get("age") >= min_age
     ]
+
+    if offset is not None:
+        if offset > len(users):
+            return []
+        out = out[offset:]
+
+    if limit is not None:
+        return out[:limit]
+
+    return out
